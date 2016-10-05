c7bc41c0b897c80892ac7c00bc4d6653ef9cdb91
Rico <rico@pgml.de>
2016-10-05 11:42:01 +0200

    allow file prefix

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const Conf = require('conf');
 
 class ElectronConfig extends Conf {
 	constructor(opts) {
-		opts = Object.assign({name: 'config'}, opts);
+		opts = Object.assign({
+			name: 'config',
+			prefix: 'json'
+		}, opts);
 		opts.cwd = (electron.app || electron.remote.app).getPath('userData');
 		opts.configName = opts.name;
+		opts.configPrefix = opts.prefix;
 		delete opts.name;
+		delete opts.prefix;
 		super(opts);
 	}
 }
